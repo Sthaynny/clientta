c570d884620d69fe3a81981d1d13e99dc84d2fa7
sthaynny <igorsthaynny@gmail.com>
2026-07-24 22:02:23 -0300

    refactor: update design_system dependency to local path
    Changed the design_system dependency in pubspec.yaml and pubspec.lock from a git source to a local path reference, improving project structure and accessibility. Updated NDK version in build.gradle.kts for compatibility.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "br.com.sthaynny.ufersa_hub"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
